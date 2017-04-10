286a9f654df42cfc4d1803a95b043b7fcda4e2d1
jymsy <jym20083617@163.com>
2017-04-10 09:23:20 +0800

    init

--- src/options.js
+++ src/options.js
@@ -3,39 +3,35 @@
     var myChart = echarts.init(element);
 
 
-    var base = +new Date(2014, 9, 3);
+    // var base = +new Date(2014, 9, 3);
+    var url = 'https://www.g-banker.com/info/getGoldPrice';
     var oneDay = 24 * 3600 * 1000;
     var date = [];
 
     var data = [Math.random() * 150];
-    var now = new Date(base);
-
-    function addData(shift) {
-        now = [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/');
-        date.push(now);
-        data.push((Math.random() - 0.4) * 10 + data[data.length - 1]);
-
-        if (shift) {
-            date.shift();
-            data.shift();
-        }
-
-        now = new Date(+new Date(now) + oneDay);
-    }
-
-    for (var i = 1; i < 100; i++) {
-        addData();
-    }
+    var now = new Date();
 
     var option = {
+        tooltip: {
+            trigger: 'axis',
+            formatter: function (params) {
+                params = params[0];
+                return params.name + '     ' + params.value;
+            },
+            axisPointer: {
+                animation: false
+            }
+        },
         xAxis: {
             type: 'category',
             boundaryGap: false,
             data: date
         },
         yAxis: {
-            boundaryGap: [0, '50%'],
-            type: 'value'
+            boundaryGap: [200, 300],
+            type: 'value',
+            min: 250,
+            max: 300
         },
         series: [
             {
@@ -53,16 +49,77 @@
     };
     myChart.setOption(option);
 
+    chrome.webRequest.onBeforeSendHeaders.addListener(
+        function(details) {
+            if (details.type === 'xmlhttprequest') {
+                details.requestHeaders.push(
+                    {
+                        name: 'Referer',
+                        value: 'https://www.g-banker.com/goldprice/'
+                    }, {
+                        name: 'Origin',
+                        value: 'https://www.g-banker.com'
+                    }, {
+                        name: 'X-Requested-With',
+                        value: 'XMLHttpRequest'
+                    }
+                );
+                  return { requestHeaders: details.requestHeaders };
+            }
+        },
+        {urls: ['https://www.g-banker.com/*']},
+        ["blocking", "requestHeaders"]
+    );
+
+    function addData(data) {
+        if (data.success === true) {
+            var time = [], price = [];
+            var date;
+            for (var value of data.priceArray) {
+                date = new Date(value.x);
+
+                time.push(date.getHours() + ':' + date.getMinutes());
+                price.push(value.y);
+            }
+            myChart.setOption({
+                xAxis: {
+                    data: time
+                },
+                series: [{
+                    name:'成交',
+                    data: price
+                }]
+            });
+        }
+        // now = [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/');
+        // date.push(now);
+        // data.push((Math.random() - 0.4) * 10 + data[data.length - 1]);
+
+        // if (shift) {
+        //     date.shift();
+        //     data.shift();
+        // }
+
+        // now = new Date(+new Date(now) + oneDay);
+    }
+
+    function getGoldPrice() {
+        var xmlhttp = new XMLHttpRequest();
+        var result;
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState===4 && xmlhttp.status===200) {
+                result = JSON.parse(xmlhttp.responseText);
+                addData(result);
+            }
+        }
+        xmlhttp.open("POST", url, true);
+        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+        xmlhttp.setRequestHeader("Accept", "*/*");
+        xmlhttp.send('queryFlag=2');
+    }
+    getGoldPrice();
     setInterval(function () {
-        addData(true);
-        myChart.setOption({
-            xAxis: {
-                data: date
-            },
-            series: [{
-                name:'成交',
-                data: data
-            }]
-        });
-    }, 500);
+        getGoldPrice();
+        
+    }, 60000);
 })(window);
